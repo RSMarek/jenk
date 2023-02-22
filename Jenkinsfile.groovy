d13a9d740956348ed6ee94caf154259aaf321bc5
Marek Szkotnicki <szkotnickimarek@gmail.com>
2023-02-22 13:43:56 +0100

    notes

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,11 +1,16 @@
 #!groovy
 
-// def rootDir = pwd()
+def rootDir = pwd()
 // def libs = load "${rootDir}@libs/lib.groovy"
 
 pipeline {
     agent any
     stages {
+        stage('load lib') {
+            steps {
+                libs = load "${rootDir}@libs/lib.groovy"
+            }
+        }
         stage('Build') {
             steps {
                 echo "Building release..."
