09009e23585d285f84e4a074416181a499389583
Marek Szkotnicki <szkotnickimarek@gmail.com>
2023-02-22 14:08:49 +0100

    notes

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,7 @@
 #!groovy
 import hudson.FilePath
 
+def libs
 
 pipeline {
     agent any
@@ -9,8 +10,9 @@ pipeline {
             steps {
                 script {
                     def workspace = env.WORKSPACE
-                    def libs = load "${workspace}/libs/lib.groovy"
+                    libs = load "${workspace}/libs/lib.groovy"
                     echo "Building lib..."
+                    libs.meth()
                 }
             }
         }
